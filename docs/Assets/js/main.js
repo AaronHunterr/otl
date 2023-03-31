6c2a969d106ea32eb818954d948dabf4c0557683
Aaron Hunter <Aaron@detroittrading.com>
2023-03-31 13:48:12 -0400

    Update main.js

--- docs/Assets/js/main.js
+++ docs/Assets/js/main.js
@@ -1,50 +1,52 @@
-function fetchDealerData() {
-  return fetch('https://drive.google.com/file/d/1JqDNyIr6WLmqKCPagBNPrF6GT7eoPCpk/export?format=csv')
-    .then(response => response.text())
-    .then(csvData => {
-      const parsedData = Papa.parse(csvData, {
-        header: true,
-        skipEmptyLines: true,
-        dynamicTyping: true
-      });
-      return parsedData.data;
-    });
-}
-
-function createDealerMap(dealerData) {
+function createDealerMap(dealers) {
   const dealerMap = new Map();
-  dealerData.forEach(dealer => dealerMap.set(dealer.DealerID, dealer));
+  dealers.forEach(dealer => dealerMap.set(dealer.DealerID, dealer));
   return dealerMap;
 }
 
-function displayInventory(data, dealerMap) {
+function displayInventory(inventory, dealerMap) {
   const inventoryContainer = document.getElementById('inventory-container');
-  const inventoryItemTemplate = document.getElementById('inventory-item');
 
-  data.forEach(row => {
-    const dealer = dealerMap.get(row.DealerID);
-    if (dealer) {
-      const inventoryItem = inventoryItemTemplate.content.cloneNode(true);
-      inventoryItem.querySelector('.inventory-image').src = `http://img.leaddelivery.net/images/${row.VIN}/Main/1.jpg?s=YOUR_SELLER_ID`;
-      inventoryItem.querySelector('.inventory-title').textContent = `${row.Year} ${row.Make} ${row.Model}`;
-      inventoryItem.querySelector('.inventory-mileage').textContent = `Mileage: ${row.Mileage}`;
-      inventoryItem.querySelector('.inventory-dealer').textContent = `Dealer: ${dealer.Name}`;
-      inventoryItem.querySelector('.inventory-detail').addEventListener('click', () => {
-        // Implement the logic to display the vehicle detail page
-      });
+  inventory.forEach(vehicle => {
+    const dealer = dealerMap.get(vehicle.DealerID);
 
-      inventoryContainer.appendChild(inventoryItem);
-    }
+    const vehicleCard = document.createElement('div');
+    vehicleCard.classList.add('vehicle-card');
+
+    const vehicleTitle = document.createElement('h3');
+    vehicleTitle.textContent = `${vehicle.Year} ${vehicle.Make} ${vehicle.Model}`;
+    vehicleCard.appendChild(vehicleTitle);
+
+    const vehicleImg = document.createElement('img');
+    vehicleImg.src = vehicle.Images.split('|')[0];
+    vehicleCard.appendChild(vehicleImg);
+
+    const vehicleMileage = document.createElement('p');
+    vehicleMileage.textContent = `Mileage: ${vehicle.Mileage}`;
+    vehicleCard.appendChild(vehicleMileage);
+
+    const dealerInfo = document.createElement('p');
+    dealerInfo.textContent = `Dealer: ${dealer.Name}, ${dealer.City}, ${dealer.State}`;
+    vehicleCard.appendChild(dealerInfo);
+
+    const viewDetailsBtn = document.createElement('button');
+    viewDetailsBtn.textContent = 'View Details';
+    viewDetailsBtn.onclick = () => {
+      // Implement vehicle display page navigation here
+    };
+    vehicleCard.appendChild(viewDetailsBtn);
+
+    inventoryContainer.appendChild(vehicleCard);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   Promise.all([
-    fetch('https://drive.google.com/file/d/1UCcOQfsuiu5OJWjiN7i1Z59CzHlwATum/export?format=csv').then(response => response.text()),
-    fetchDealerData()
+    fetch('https://dl.dropboxusercontent.com/s/8oke8b9sz8lvo57/dtcinventory.csv').then(response => response.text()),
+    fetch('https://dl.dropboxusercontent.com/s/qy1r1of8z1pf5iu/dtcdealers.csv').then(response => response.text())
   ])
-    .then(([csvInventoryData, dealerData]) => {
-      const dealerMap = createDealerMap(dealerData);
+    .then(([csvInventoryData, csvDealerData]) => {
+      const dealerMap = createDealerMap(Papa.parse(csvDealerData, {header: true, skipEmptyLines: true, dynamicTyping: true}).data);
       const parsedInventoryData = Papa.parse(csvInventoryData, {header: true, skipEmptyLines: true, dynamicTyping: true});
       displayInventory(parsedInventoryData.data, dealerMap);
     })
