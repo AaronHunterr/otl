e7606374864ff0ee8ddf37542cee9fd371e98363
Aaron Hunter <Aaron@detroittrading.com>
2023-04-07 14:09:09 -0400

    Update script.js

--- docs/Assets/script.js
+++ docs/Assets/script.js
@@ -19,73 +19,77 @@
  * *****************************************************************************
  */
 
-var LxLoader = (function() {
-  function LxLoader() {
-    this.debug = false;
-    this.dev = false;
-    this.testHost = "";
-    this.queue = window.VinAds || [];
-    this.process();
-  }
-  LxLoader.prototype.setPage = function(page) {
-    console.trace("Set Page");
-    this.page = page;
-    if (page.debug) {
-      this.debug = page.debug;
+var LxLoader = (function () {
+    function LxLoader() {
+        this.debug = false;
+        this.dev = false;
+        this.testHost = "";
+        this.queue = window.VinAds || [];
+        this.process();
     }
-    if (page.dev) {
-      this.dev = page.dev;
-      if (page.host) {
-        this.testHost = page.host;
-      }
-    }
-  };;;
-  LxLoader.prototype.domain = function() {
-    if (this.dev) {
-      if (this.testHost != "") {
-        return this.testHost;
-      }
-      return "https://dev-ads.lotlinx.com";
-    }
-    return "https://ads.lotlinx.com";
-  };;
-  LxLoader.prototype.post = function(url, body, handler) {
-    console.log('Payload:', body);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(body);
-    xhr.onload = function() {
-      if (xhr.status != 200) {
-        handler.failure(xhr.response);
-        return;
-      }
-      try {
-        var resp = JSON.parse(xhr.response);
-        if (typeof resp.status != 'undefined') {
-          if (resp.status == 'ok') {
-            if (typeof resp.data != 'undefined') {
-              handler.success(resp);
-              return;
+    LxLoader.prototype.setPage = function (page) {
+        console.trace("Set Page");
+        this.page = page;
+        if (page.debug) {
+            this.debug = page.debug;
+        }
+        if (page.dev) {
+            this.dev = page.dev;
+            if (page.host) {
+                this.testHost = page.host;
             }
-          }
         }
-        handler.failure(resp);
-        return;
-      } catch (err) {
-        console.error(err);
-      }
-      handler.failure({
-        "status": "error",
-        "data": {
-          "message": "Unhandled error from server",
-          "rawResponse": xhr.response
-        }
-      });
-      return;
     };
-  };;
-  LxLoader.prototype.getVins = function (operation) {
+    ;
+    ;
+    LxLoader.prototype.domain = function () {
+        if (this.dev) {
+            if (this.testHost != "") {
+                return this.testHost;
+            }
+            return "https://dev-ads.lotlinx.com";
+        }
+        return "https://ads.lotlinx.com";
+    };
+    ;
+    LxLoader.prototype.post = function (url, body, handler) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send(body);
+        xhr.onload = function () {
+            if (xhr.status != 200) {
+                handler.failure(xhr.response);
+                return;
+            }
+            try {
+                var resp = JSON.parse(xhr.response);
+                if (typeof resp.status != 'undefined') {
+                    if (resp.status == 'ok') {
+                        if (typeof resp.data != 'undefined') {
+                            handler.success(resp);
+                            return;
+                        }
+                    }
+                }
+                handler.failure(resp);
+                return;
+            }
+            catch (err) {
+                console.error(err);
+            }
+            handler.failure({
+                "status": "error",
+                "data": {
+                    "message": "Unhandled error from server",
+                    "rawResponse": xhr.response
+                }
+            });
+            return;
+        };
+    };
+    ;
+    LxLoader.prototype.getVins = function (operation) {
         console.trace("Get Vins");
         var url = this.domain() + "/v2/getVins";
         var body = JSON.stringify({
@@ -108,190 +112,199 @@ var LxLoader = (function() {
         this.post(url, body, handler);
     };
     ;
-  LxLoader.prototype.getDetails = function(operation) {
-    console.trace("Get Details");
-    var url = this.domain() + "/v2/getDetails";
-    var body = JSON.stringify({
-      "page": this.page,
-      "op": operation
-    });
-    if (typeof operation.success != "function") {
-      console.error("No function defined for success on call");
-      return;
-    }
-    if (typeof operation.failure != "function") {
-      operation.failure = function(resp) {
-        console.trace(resp.data.message);
-      };
-    }
-    var handler = {
-      success: operation.success,
-      failure: operation.failure
-    };
-    this.post(url, body, handler);
-  };;
-  LxLoader.prototype.buildAd = function(operation) {
-    console.trace("Build Ad");
-    var url = this.domain() + "/v2/buildAd";
-    var body = JSON.stringify({
-      "page": this.page,
-      "op": operation
-    });
-    var fun = this.populateFrame;
-    if (typeof operation.failure != "function") {
-      operation.failure = function(resp) {
-        console.trace(resp.data.message);
-      };
-    }
-    var handler = {
-      success: function(resp) {
-        if (resp.data.url != "undefined") {
-          fun(operation.container, resp.data.url, operation.width, operation.height);
-          return;
-        } else {
-          console.log("Error parsing response url");
+    LxLoader.prototype.getDetails = function (operation) {
+        console.trace("Get Details");
+        var url = this.domain() + "/v2/getDetails";
+        var body = JSON.stringify({
+            "page": this.page,
+            "op": operation
+        });
+        if (typeof operation.success != "function") {
+            console.error("No function defined for success on call");
+            return;
         }
-      },
-      failure: operation.failure
-    };
-    this.post(url, body, handler);
-  };;
-  LxLoader.prototype.buildCustom = function(operation) {
-    console.trace("Build Custom");
-    var url = this.domain() + "/v2/buildCustom";
-    var body = JSON.stringify({
-      "page": this.page,
-      "op": operation
-    });
-    var success = operation.success;
-    var failure = operation.failure;
-    var callback = operation.callback;
-    if (typeof success != "function") {
-      if (typeof callback != "function") {
-        console.error("Build Custom operation requires success or callback function");
-        return;
-      } else {
-        success = function(resp) {
-          console.log('Response:', resp);
-          console.trace(resp.data.message);
-          callback(resp.data);
+        if (typeof operation.failure != "function") {
+            operation.failure = function (resp) {
+                console.trace(resp.data.message);
+            };
+        }
+        var handler = {
+            success: operation.success,
+            failure: operation.failure
         };
-        failure = function(resp) {
-          console.trace(resp.data.message);
-          callback("");
-        };
-      }
-    } else {
-      if (typeof failure != "function") {
-        failure = function(resp) {
-          console.trace(resp.data.message);
-        };
-      }
-    }
-    var handler = {
-      success: success,
-      failure: failure
+        this.post(url, body, handler);
     };
-    this.post(url, body, handler);
-  };;
-  LxLoader.prototype.managedLoadAd = function(operation) {
-    console.trace("Managed Load Ad");
-    var url = this.domain() + "/v2/managedLoadAd";
-    var body = JSON.stringify({
-      "page": this.page,
-      "op": operation
-    });
-    if (typeof operation.lineItem == "undefined") {
-      console.error("LineItem is required for managed load");
-      return;
-    }
-    var fun = this.populateFrame;
-    var handler = {
-      success: function(resp) {
-        if (resp.data.url != "undefined") {
-          fun(operation.container, resp.data.url, operation.width, operation.height);
-          return;
-        } else {
-          console.log("Error parsing response url");
+    ;
+    LxLoader.prototype.buildAd = function (operation) {
+        console.trace("Build Ad");
+        var url = this.domain() + "/v2/buildAd";
+        var body = JSON.stringify({
+            "page": this.page,
+            "op": operation
+        });
+        var fun = this.populateFrame;
+        if (typeof operation.failure != "function") {
+            operation.failure = function (resp) {
+                console.trace(resp.data.message);
+            };
         }
-      },
-      failure: function(resp) {
-        console.error(resp.data.message);
-      }
+        var handler = {
+            success: function (resp) {
+                if (resp.data.url != "undefined") {
+                    fun(operation.container, resp.data.url, operation.width, operation.height);
+                    return;
+                }
+                else {
+                    console.log("Error parsing response url");
+                }
+            },
+            failure: operation.failure
+        };
+        this.post(url, body, handler);
     };
-    this.post(url, body, handler);
-  };;
-  LxLoader.prototype.populateFrame = function(container, sourceUrl, width, height) {
-    var frame = document.createElement("iframe");
-    frame.setAttribute("scrolling", "no");
-    frame.style.border = "none";
-    if (typeof width == "undefined" || width <= 0)
-      frame.style.width = "100%";
-    else
-      frame.style.width = width + "px";
-    if (typeof height == "undefined" || height <= 0)
-      frame.style.height = "100%";
-    else
-      frame.style.height = height + "px";
-    frame.style.overflow = "hidden";
-    frame.src = sourceUrl;
-    document.getElementById(container).appendChild(frame);
-  };;
-  LxLoader.prototype.process = function() {
-    if (typeof this.page == "undefined") {
-      var pageIsSet = false;
-      for (var i = 0; i < this.queue.length; i++) {
-        if (typeof this.queue[i].op != 'undefined' &&
-          this.queue[i].op == 'setPage') {
-          var operation = this.queue.splice(i, 1);
-          this.setPage(operation[0]);
-          pageIsSet = true;
-          break;
+    ;
+    LxLoader.prototype.buildCustom = function (operation) {
+        console.trace("Build Custom");
+        var url = this.domain() + "/v2/buildCustom";
+        var body = JSON.stringify({
+            "page": this.page,
+            "op": operation
+        });
+        var success = operation.success;
+        var failure = operation.failure;
+        var callback = operation.callback;
+        if (typeof success != "function") {
+            if (typeof callback != "function") {
+                console.error("Build Custom operation requires success or callback function");
+                return;
+            }
+            else {
+                success = function (resp) {
+                    console.trace(resp.data.message);
+                    callback(resp.data);
+                };
+                failure = function (resp) {
+                    console.trace(resp.data.message);
+                    callback("");
+                };
+            }
         }
-      }
-      if (!pageIsSet)
-        return;
-    }
-    while (this.queue.length > 0) {
-      var e = this.queue.shift();
-      if (typeof e.op != 'undefined') {
-        switch (e.op) {
-          case "setPage":
-            this.setPage(e);
-            break;
-          case "getVins":
-            this.getVins(e);
-            break;
-          case "buildAd":
-            this.buildAd(e);
-            break;
-          case "getDetails":
-            this.getDetails(e);
-            break;
-          case "buildCustom":
-            this.buildCustom(e);
-            break;
-          case "managedLoadAd":
-            this.managedLoadAd(e);
-            break;
+        else {
+            if (typeof failure != "function") {
+                failure = function (resp) {
+                    console.trace(resp.data.message);
+                };
+            }
         }
-        continue;
-      }
-    }
-  };
-  LxLoader.prototype.push = function() {
-    var ops = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-      ops[_i] = arguments[_i];
-    }
-    for (var i = 0; i < ops.length; i++) {
-      console.trace("Push");
-      this.queue.push(ops[i]);
-    }
-    this.process();
-  };;
-  return LxLoader;
+        var handler = {
+            success: success,
+            failure: failure
+        };
+        this.post(url, body, handler);
+    };
+    ;
+    LxLoader.prototype.managedLoadAd = function (operation) {
+        console.trace("Managed Load Ad");
+        var url = this.domain() + "/v2/managedLoadAd";
+        var body = JSON.stringify({
+            "page": this.page,
+            "op": operation
+        });
+        if (typeof operation.lineItem == "undefined") {
+            console.error("LineItem is required for managed load");
+            return;
+        }
+        var fun = this.populateFrame;
+        var handler = {
+            success: function (resp) {
+                if (resp.data.url != "undefined") {
+                    fun(operation.container, resp.data.url, operation.width, operation.height);
+                    return;
+                }
+                else {
+                    console.log("Error parsing response url");
+                }
+            },
+            failure: function (resp) {
+                console.error(resp.data.message);
+            }
+        };
+        this.post(url, body, handler);
+    };
+    ;
+    LxLoader.prototype.populateFrame = function (container, sourceUrl, width, height) {
+        var frame = document.createElement("iframe");
+        frame.setAttribute("scrolling", "no");
+        frame.style.border = "none";
+        if (typeof width == "undefined" || width <= 0)
+            frame.style.width = "100%";
+        else
+            frame.style.width = width + "px";
+        if (typeof height == "undefined" || height <= 0)
+            frame.style.height = "100%";
+        else
+            frame.style.height = height + "px";
+        frame.style.overflow = "hidden";
+        frame.src = sourceUrl;
+        document.getElementById(container).appendChild(frame);
+    };
+    ;
+    LxLoader.prototype.process = function () {
+        if (typeof this.page == "undefined") {
+            var pageIsSet = false;
+            for (var i = 0; i < this.queue.length; i++) {
+                if (typeof this.queue[i].op != 'undefined'
+                    && this.queue[i].op == 'setPage') {
+                    var operation = this.queue.splice(i, 1);
+                    this.setPage(operation[0]);
+                    pageIsSet = true;
+                    break;
+                }
+            }
+            if (!pageIsSet)
+                return;
+        }
+        while (this.queue.length > 0) {
+            var e = this.queue.shift();
+            if (typeof e.op != 'undefined') {
+                switch (e.op) {
+                    case "setPage":
+                        this.setPage(e);
+                        break;
+                    case "getVins":
+                        this.getVins(e);
+                        break;
+                    case "buildAd":
+                        this.buildAd(e);
+                        break;
+                    case "getDetails":
+                        this.getDetails(e);
+                        break;
+                    case "buildCustom":
+                        this.buildCustom(e);
+                        break;
+                    case "managedLoadAd":
+                        this.managedLoadAd(e);
+                        break;
+                }
+                continue;
+            }
+        }
+    };
+    LxLoader.prototype.push = function () {
+        var ops = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            ops[_i] = arguments[_i];
+        }
+        for (var i = 0; i < ops.length; i++) {
+            console.trace("Push");
+            this.queue.push(ops[i]);
+        }
+        this.process();
+    };
+    ;
+    return LxLoader;
 }());
-window.VinAds = (function() {
-  return new LxLoader();
+window.VinAds = (function () {
+    return new LxLoader();
 })();
